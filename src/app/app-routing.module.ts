c3d3577ca9608014b44d6fb1c79d6e0debdaadbf
theGeekkit <sonofgondolin16@gmail.com>
2022-12-31 13:30:56 -0600

    bckgrnd play 12-31-22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { BriarheartComponent } from './briarheart/briarheart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'briarheart',
+    component: BriarheartComponent
+  },
+  {
+    path: 'home',
+    component: AppComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'home';
   title = 'campaign_journals';
+  imageUrl = 'url(https://i.ibb.co/0nyBdyj/pexels-pixabay-235985.jpg)';
+
+  constructor(private router: Router) {
+    router.events
+    .pipe(filter(event => event instanceof NavigationStart), map(e => e as NavigationStart))
+    .subscribe((event: NavigationStart) => {
+      console.log(event)
+      if(event.url === '/home') {
+        this.imageUrl = 'url(https://i.ibb.co/0nyBdyj/pexels-pixabay-235985.jpg)'
+      } else if(event.url === '/briarheart') {
+        this.imageUrl = 'url(https://i.ibb.co/b2rqPz1/92f59995fb654b6f476e532970a164ce.jpg)'
+      }
+    }
+
+    )};
 }
